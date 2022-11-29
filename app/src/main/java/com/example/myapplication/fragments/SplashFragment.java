a238db016388cefbdae6d878aa07e39575b08505
José Miguel <jcanov@unsa.edu.pe>
2022-11-29 11:15:39 -0500

    agregando fragment Splash

---
+++ app/src/main/java/com/example/myapplication/fragments/SplashFragment.java
@@ -0,0 +1,50 @@
+package com.example.myapplication.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.myapplication.R;
+
+
+public class SplashFragment extends Fragment {
+
+    LottieAnimationView lottie;
+    public SplashFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        lottie = view.findViewById(R.id.animationView);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Navigation.findNavController(view).navigate(R.id.login_Fragment);
+            }
+        },3000);
+    }
+}
